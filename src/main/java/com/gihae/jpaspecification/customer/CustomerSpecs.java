92f2e39fb06d040d77a05556509781eeed5fad8e
xcelxlorx <xcelxlorx@gmail.com>
2023-11-09 00:09:57 +0900

    feat: Add lessThan criteria to findAll

--- src/main/java/com/gihae/jpaspecification/customer/CustomerSpecs.java
+++ src/main/java/com/gihae/jpaspecification/customer/CustomerSpecs.java
@@ -1,4 +1,24 @@
 package com.gihae.jpaspecification.customer;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RequiredArgsConstructor
 public class CustomerSpecs {
+
+    private final CustomerRepository customerRepository;
+
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll(isLongTermCustomer());
+    }
+
+    public Specification<Customer> isLongTermCustomer(){
+        return (root, query, builder) -> {
+            LocalDate date = LocalDate.now().minusYears(2);
+            return builder.lessThan(root.get(Customer.createdAt), date);
+        };
+    }
 }
