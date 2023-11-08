ea4bf25205664068a3b00585d062e1ddbb1053a1
xcelxlorx <xcelxlorx@gmail.com>
2023-11-08 23:58:35 +0900

    feat: Add customer repository

---
+++ src/main/java/com/gihae/jpaspecification/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.gihae.jpaspecification;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long>, JpaSpecificationExecutor<Customer> {
+
+    List<Customer> findAll(Specification<Customer> spec);
+}
