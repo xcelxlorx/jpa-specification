f45bfdb7b0414149a3f3a54d430b630662a88cc9
xcelxlorx <xcelxlorx@gmail.com>
2023-11-08 23:37:29 +0900

    init: Init project

---
+++ src/main/java/com/gihae/jpaspecification/JpaSpecificationApplication.java
@@ -0,0 +1,13 @@
+package com.gihae.jpaspecification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSpecificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSpecificationApplication.class, args);
+	}
+
+}
