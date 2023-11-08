6b82b72cc7704108ee42cd62808c3e884fa91ff4
xcelxlorx <xcelxlorx@gmail.com>
2023-11-08 23:58:14 +0900

    feat: Add customer entity

---
+++ src/main/java/com/gihae/jpaspecification/Customer.java
@@ -0,0 +1,16 @@
+package com.gihae.jpaspecification;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+}
